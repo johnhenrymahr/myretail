e402489a26c7abd6c90593ed51d9b9234ad9059d
John  Mahr <john@johnhenrymahr.com>
2018-01-14 14:10:23 -0600

    add hot loader integration  for redux

--- app/state/configureStore.js
+++ app/state/configureStore.js
@@ -13,6 +13,12 @@ export default function configureStore (preloadedState) {
       window.devToolsExtension ? window.devToolsExtension() : f => f
     )
   )
-
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const nextReducer = require('./reducers').default
+      store.replaceReducer(nextReducer)
+   
+    })
+  }
   return store
 }
